1537edb4979f9bfdd78e6183d157e548a9d21d6c
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-01-25 16:17:40 +0700

    proses pendaftaran mahasiswa

--- daftar.php
+++ daftar.php
@@ -1,7 +1,23 @@
 <?php
-session_start();
-if (isset($_SESSION['iduser'])) {
-    header('location:./index.php');
+require 'config/database.php';
+$error = '';
+if (isset($_POST['tambah'])) {
+    $nama = $_POST['nama'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query_username = "SELECT * FROM mahasiswa WHERE username_mhs='$username'";
+    $cek_username = $connect->query($query_username);
+    if ($nama == '' || $username == '' || $password == '') {
+        $error = 'Nama lengkap, Username, atau Password tidak boleh kosong';
+    } else if ($cek_username->num_rows > 0) {
+        $error = 'Username sudah digunakan';
+    } else {
+        $query = "INSERT INTO mahasiswa(nama_mhs,username_mhs,password_mhs) VALUES('$nama','$username',md5('$password'))";
+        if ($connect->query($query) === true) {
+            echo '<script>window.location.replace("login.php")</script>';
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -10,7 +26,7 @@ if (isset($_SESSION['iduser'])) {
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta charset="utf-8" />
-    <title>Login - SPK SAW</title>
+    <title>Daftar - SPK SAW</title>
 
     <meta name="description" content="User login page" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
@@ -19,15 +35,6 @@ if (isset($_SESSION['iduser'])) {
     <link rel="stylesheet" href="assets/css/fonts.googleapis.com.css" />
     <link rel="stylesheet" href="assets/css/ace.min.css" />
     <link rel="stylesheet" href="assets/css/ace-rtl.min.css" />
-    <style>
-        .login-box .toolbar>div:first-child {
-            text-align: center;
-        }
-
-        .login-box .toolbar>div {
-            width: 100%;
-        }
-    </style>
 </head>
 
 <body class="login-layout">
@@ -44,55 +51,48 @@ if (isset($_SESSION['iduser'])) {
                             </h1>
                             <h4 class="blue" id="id-company-text">SPK Metode SAW</h4>
                         </div>
-
                         <div class="space-6"></div>
-
                         <div class="position-relative">
-                            <div id="login-box" class="login-box visible widget-box no-border">
+                            <div class="signup-box visible widget-box no-border">
                                 <div class="widget-body">
                                     <div class="widget-main">
-                                        <h4 class="header blue lighter bigger">
-                                            <i class="ace-icon fa fa-coffee green"></i>
-                                            Silahkan login
+                                        <h4 class="header green lighter bigger">
+                                            <i class="ace-icon fa fa-users blue"></i>
+                                            Pendaftaran Calon Mahasiswa
                                         </h4>
-
-                                        <div class="space-6"></div>
-
-                                        <?= isset($_SESSION['message']) ? '<div class="alert alert-danger">' . $_SESSION['message'] . '</div>' : '' ?>
-
-                                        <form method="post" action="proses-login.php">
+                                        <?= $error != '' ? '<div class="alert alert-danger">' . $error . '</div>' : '' ?>
+                                        <form action="" method="POST">
                                             <fieldset>
                                                 <label class="block clearfix">
                                                     <span class="block input-icon input-icon-right">
-                                                        <input type="text" name="username" class="form-control" placeholder="Username" />
+                                                        <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap" />
                                                         <i class="ace-icon fa fa-user"></i>
                                                     </span>
                                                 </label>
-
                                                 <label class="block clearfix">
                                                     <span class="block input-icon input-icon-right">
-                                                        <input type="password" name="password" class="form-control" placeholder="Password" />
+                                                        <input type="text" class="form-control" name="username" placeholder="Username" />
+                                                        <i class="ace-icon fa fa-user"></i>
+                                                    </span>
+                                                </label>
+                                                <label class="block clearfix">
+                                                    <span class="block input-icon input-icon-right">
+                                                        <input type="password" class="form-control" name="password" placeholder="Password" />
                                                         <i class="ace-icon fa fa-lock"></i>
                                                     </span>
                                                 </label>
-
-                                                <div class="space"></div>
-
+                                                <div class="space-24"></div>
                                                 <div class="clearfix">
-                                                    <button type="submit" class="width-35 pull-right btn btn-sm btn-primary">
-                                                        <i class="ace-icon fa fa-key"></i>
-                                                        <span class="bigger-110">Login</span>
+                                                    <button type="submit" name="tambah" class="width-100 pull-right btn btn-sm btn-success">
+                                                        <span class="bigger-110">Register</span>
+                                                        <i class="ace-icon fa fa-arrow-right icon-on-right"></i>
                                                     </button>
                                                 </div>
-
-                                                <div class="space-4"></div>
                                             </fieldset>
                                         </form>
                                     </div>
-                                    <div class="toolbar clearfix">
-                                        <div>
-                                            <a href="daftar.php" class="forgot-password-link">Pendaftaran Calon Mahasiswa <i class="ace-icon fa fa-arrow-right"></i></a>
-                                        </div>
+                                    <div class="toolbar center">
+                                        <a href="login.php" class="back-to-login-link"><i class="ace-icon fa fa-arrow-left"></i> Kembali ke login</a>
                                     </div>
                                 </div>
                             </div>
