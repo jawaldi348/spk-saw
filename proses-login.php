136cb47ee89ca35147c4c2d51464b9f01759edd9
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-01-25 16:18:34 +0700

    tambah proses login untuk mahasiswa

--- proses-login.php
+++ proses-login.php
@@ -7,12 +7,23 @@ if (empty($user || $pass)) {
     $_SESSION['message'] = 'Username atau password tidak boleh kosong';
     header('location:./login.php');
 } else {
-    $query = "SELECT * FROM user WHERE username='$user' AND password=md5('$pass')";
-    $execute = $connect->query($query);
-    if ($execute->num_rows > 0) {
-        $data = $execute->fetch_array(MYSQLI_ASSOC);
+    // Cek Data User
+    $query_user = "SELECT * FROM user WHERE username='$user' AND password=md5('$pass')";
+    $cek_user = $connect->query($query_user);
+    // Cek Data Mahasiswa
+    $query_mhs = "SELECT * FROM mahasiswa WHERE username_mhs='$user' AND password_mhs=md5('$pass')";
+    $cek_mhs = $connect->query($query_mhs);
+    if ($cek_user->num_rows > 0) {
+        $data = $cek_user->fetch_array(MYSQLI_ASSOC);
         unset($_SESSION['message']);
         $_SESSION['iduser'] = $data['id_user'];
+        $_SESSION['level'] = 'admin';
+        header('location:./index.php');
+    } elseif ($cek_mhs->num_rows > 0) {
+        $data = $cek_mhs->fetch_array(MYSQLI_ASSOC);
+        unset($_SESSION['message']);
+        $_SESSION['iduser'] = $data['kode_mhs'];
+        $_SESSION['level'] = 'mhs';
         header('location:./index.php');
     } else {
         $_SESSION['message'] = 'Akun yang diinputkan tidak terdaftar';
