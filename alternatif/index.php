ac6027b442611838496a9bc7265f5ce56a4e9c50
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-02-01 13:45:55 +0700

    panggil data mahasiswa dengan ajax

--- alternatif/index.php
+++ alternatif/index.php
@@ -1,50 +1,29 @@
-<?php
-require 'alternatif/data.php';
-$data = new data();
-$data_mahasiswa = $data->data_mahasiswa();
-?>
 <div class="table-header">Data Mahasiswa
     <!-- <a href="./?page=alternatif&proses=tambah" class="btn btn-xs btn-success"><i class="ace-icon glyphicon glyphicon-plus bigger-80"></i> Tambah</a> -->
 </div>
 <div>
-    <table id="dynamic-table" class="table table-striped table-bordered table-hover">
-        <thead>
-            <tr>
-                <th class="center" width="5%">No</th>
-                <th>No Pendaftaran</th>
-                <th>Nama</th>
-                <?php foreach ($data_mahasiswa['kriteria'] as $kriteria) { ?>
-                    <td class="text-center"><?= $kriteria['nama_kriteria'] ?></td>
-                <?php } ?>
-                <th class="center" width="8%">Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $no = 1;
-            foreach ($data_mahasiswa['mahasiswa'] as $mahasiswa) { ?>
-                <tr>
-                    <td class="center"><?= $no ?></td>
-                    <td><?= $mahasiswa['nodaftar_mhs'] ?></td>
-                    <td><?= $mahasiswa['nama_mhs'] ?></td>
-                    <?php foreach ($mahasiswa['kriteria'] as $nilai_kriteria) { ?>
-                        <td class="text-center">
-                            <span class="badge badge-danger" style="margin-bottom: 8px">
-                                <?= $nilai_kriteria['bobot_subkriteria'] ?>
-                            </span><br />
-                            <?= $nilai_kriteria['nama_subkriteria'] ?>
-                        </td>
-                    <?php } ?>
-                    <td class="center">
-                        <a class="green" href="./?page=alternatif&proses=edit&id=<?= $mahasiswa['kode_mhs'] ?>">
-                            <i class="ace-icon glyphicon glyphicon-edit bigger-130"></i>
-                        </a>
-                        <a class="red" href="./alternatif/delete.php?id=<?= $mahasiswa['kode_mhs'] ?>" onclick="return confirm('Anda yakin hapus data ini?');">
-                            <i class="ace-icon glyphicon glyphicon-trash bigger-130"></i>
-                        </a>
-                    </td>
-                </tr>
-            <?php $no++;
-            } ?>
-        </tbody>
-    </table>
+    <?php $query_tahun = "SELECT * FROM tahun_akademik WHERE status_tahun='1'";
+    $execute_tahun = $connect->query($query_tahun);
+    $data_tahun = $execute_tahun->fetch_array(MYSQLI_ASSOC); ?>
+    <input type="hidden" name="idtahun" id="idtahun" value="<?= $data_tahun['id_tahun'] ?>">
+    <div id="data-tabel"></div>
 </div>
+<script>
+    $(document).ready(function() {
+        var idtahun = $('#idtahun').val();
+        data_kelas_kuliah(idtahun);
+    });
+
+    function data_kelas_kuliah(idtahun) {
+        $.ajax({
+            url: 'alternatif/data.php',
+            method: 'get',
+            data: {
+                idtahun: idtahun
+            },
+            success: function(resp) {
+                $('#data-tabel').html(resp);
+            }
+        })
+    }
+</script>
