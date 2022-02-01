3647d902c5c2a9f43fa3f9e087229a1a5d21aa31
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-02-01 13:47:09 +0700

    filter data mahasiswa berdasarkan tahun akademik dengan ajax

--- alternatif/index.php
+++ alternatif/index.php
@@ -1,4 +1,14 @@
 <div class="table-header">Data Mahasiswa
+    <div class="pull-right" style="padding: 2px;">
+        <select name="tahun" id="tahun" class="form-control">
+            <option value="">Pilih Tahun Akademik</option>
+            <?php $query_tahun = "SELECT * FROM tahun_akademik";
+            $result_tahun = $connect->query($query_tahun);
+            while ($data_tahun = $result_tahun->fetch_array(MYSQLI_ASSOC)) { ?>
+                <option value="<?= $data_tahun['id_tahun'] ?>"><?= $data_tahun['nama_tahun'] ?></option>
+            <?php } ?>
+        </select>
+    </div>
     <!-- <a href="./?page=alternatif&proses=tambah" class="btn btn-xs btn-success"><i class="ace-icon glyphicon glyphicon-plus bigger-80"></i> Tambah</a> -->
 </div>
 <div>
@@ -26,4 +36,9 @@
             }
         })
     }
+
+    $('#tahun').change(function() {
+        var idtahun = $(this).val();
+        data_kelas_kuliah(idtahun);
+    });
 </script>
