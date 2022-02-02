c5aebd4b3fe77620e3cec98ab50129ec9a169606
lauraaprilaini <ara.april04@gmail.com>
2022-02-02 11:47:18 +0700

    update halaman home

--- layout/home.php
+++ layout/home.php
@@ -1,1 +1,11 @@
-home
+<div class="text-center">
+	<h2>Selamat Datang Di Sistem Informasi Seleksi Penerima Kartu Indonesia Pintar IBTPI</h2>
+	<div class="row">
+		<div class="col-md-6">
+			<img src="./assets/images/banner1.jpg" class="img-responsive">
+		</div>
+		<div class="col-md-6">
+			<img src="./assets/images/banner2.jpeg" class="img-responsive" style="height: 295px">
+		</div>
+	</div>
+</div>
